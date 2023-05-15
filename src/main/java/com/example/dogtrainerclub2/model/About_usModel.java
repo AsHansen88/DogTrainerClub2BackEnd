a704836fd3bdcd756717314322c0954aa74bed5f
AsHansen88 <Ande970s@stud.kea.dk>
2023-05-15 13:40:04 +0200

    About us backend nu med phononumber og photo

--- src/main/java/com/example/dogtrainerclub2/model/About_usModel.java
+++ src/main/java/com/example/dogtrainerclub2/model/About_usModel.java
@@ -7,16 +7,19 @@ import jakarta.persistence.*;
 @Table(name ="members")
 public class About_usModel {
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
-@Column(nullable = false)
-private String firstname;
-@Column(nullable = false)
-private String lastname;
-@Column(nullable = false, unique = true)
-private String email;
-
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(nullable = false)
+  private String firstname;
+  @Column(nullable = false)
+  private String lastname;
+  @Column(nullable = false)
+  private int phonenumber;
+  @Column(nullable = false, unique = true)
+  private String email;
+  @Column(name = "photo")
+  private String photo;
 
   public About_usModel(Long id, String firstname, String lastname, String email) {
     this.id = id;
@@ -59,4 +62,21 @@ private String email;
   public void setEmail(String email) {
     this.email = email;
   }
+
+  public int getPhonenumber() {
+    return phonenumber;
+  }
+
+  public void setPhonenumber(int phonenumber) {
+    this.phonenumber = phonenumber;
+  }
+
+  public String getPhoto() {
+    return photo;
+  }
+
+  public void setPhoto(String photo) {
+    this.photo = photo;
+  }
 }
+
